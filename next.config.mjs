98bfdb5f5509624f762a5e300bb17f112eb1a9db
mohamed elkheateb <156317154+mohamed10987@users.noreply.github.com>
2026-08-30 15:59:07 +0200

    Static export for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    localPatterns: [
-      {
-        pathname: "/images/**",
-      },
-    ],
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    unoptimized: true,
   },
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: false,
-  },
 };
 
 export default nextConfig;
